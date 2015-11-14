e175893f381f3bc2e40c7edb74c1814dc6284add
paco <pakokrew@gmail.com>
2015-11-14 22:40:23 +0100

    SRC titles

--- server/config/config.js
+++ server/config/config.js
@@ -3,6 +3,6 @@ var Config = {};
 Config.hostname = "song-bridge.herokuapp.com";
 Config.port = null;
 
-Config.host = 'http://' + Config.hostname + (Config.port ? ':' + Config.port : '');
+Config.host = 'http://' + Config.hostname + (Config.port ? (':' + Config.port) : '');
 
 module.exports = Config;
