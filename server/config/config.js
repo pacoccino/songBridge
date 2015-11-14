4319321233bd7d720d134eaa25d1a6a6773c7a15
paco <pakokrew@gmail.com>
2015-11-14 22:47:30 +0100

    SRC config protocol

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,9 @@
 var Config = {};
 
-Config.hostname = "localhost";
-Config.port = 8080;
+Config.protocol = "http";
+Config.hostname = "song-bridge.herokuapp.com";
+Config.port = null;
 
-Config.host = 'http://' + Config.hostname + ':' + Config.port;
+Config.host = Config.protocol + '://' + Config.hostname + (Config.port ? (':' + Config.port) : '');
 
 module.exports = Config;
