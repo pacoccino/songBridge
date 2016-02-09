47a6a09a3404908277c21211c4d1b716f7806107
Paco pac <pakokrew@gmail.com>
2016-02-09 22:12:21 +0100

    begin Soundcloud Lobby

---
+++ server/crawlers/lobbyCrawler.js
@@ -0,0 +1,38 @@
+var async = require('async');
+
+var Artist = require('../scmodels/artist');
+var ArtistScrapper = require('./artistScrapper');
+
+class LobbyCrawler {
+    constructor(soundcloud) {
+        this.soundCloud = soundcloud;
+    }
+
+    crawlAllArtists(callback) {
+        var self = this;
+        var scrapper = new ArtistScrapper(self.soundcloud);
+        var limitArtist = 10;
+        var limitLobbies = 10;
+
+        Artist.find({}, function onArtists(err, artists) {
+            if(err) return callback(err);
+
+            async.eachLimit(artists, limitArtist, forEachArtist);
+        });
+
+        function forEachArtist(artist) {
+            scrapper.getMissingSounds(artist.sc_id_artist, function onMissingSongs(missingSongs) {
+                artist.getLobbies(function onLobbies(artistLobbies) {
+                    async.eachLimit(artistLobbies, limitLobbies, function(lobby) {
+                        var playlistId = lobby.sc_id_playlist;
+                        User.findById(lobby.sc_id_user, function(err, user) {
+                            self.soundCloud.addToPlaylistTop(playlistId, user, missingSongs);
+                        });
+                    });
+                });
+            });
+        }
+    }
+}
+
+module.exports = LobbyCrawler;
---
+++ server/scmodels/artist.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var _ = require('lodash');
+
+var artistSchema = mongoose.Schema({
+        sc_id_artist: {type: String, index: true},
+        lastFetchedSong: String
+    },
+    {
+        strict: true
+    }
+);
+
+artistSchema.methods = {
+    getLobbies: function(callback) {
+        // TODO get all lobbies containing this artist
+        callback([]);
+    },
+    updateLastSong: function(songId, callback) {
+        if(typeof songId !== string) {
+            return callback("Wrong song id");
+        }
+
+        this.lastFetchedSong = songId;
+        this.save(callback);
+    }
+};
+
+var Lobby = mongoose.model('Artist', artistSchema);
+
+module.exports = Lobby;
