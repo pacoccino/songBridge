add9bc4ff8881021a18cee5d000b9eda0c45d84f
paco <pakokrew@gmail.com>
2016-02-12 18:37:55 +0100

    Lobby by lobby crawler

--- server/crawlers/lobbyCrawler.js
+++ server/crawlers/lobbyCrawler.js
@@ -1,37 +1,105 @@
-var async = require('async');
+var _ = require('lodash');
 
-var Artist = require('../scmodels/artist');
-var ArtistScrapper = require('./artistScrapper');
+var Lobby = require('../scmodels/lobby');
+
+// TODO mieux gerer les erreurs, quand s'arreter ?
 
 class LobbyCrawler {
-    constructor(soundcloud) {
+    constructor(soundcloud, cacheWrapper) {
         this.soundCloud = soundcloud;
+        this.cacheWrapper = cacheWrapper;
     }
 
-    crawlAllArtists(callback) {
+    updateLobby(lobby, callback) {
         var self = this;
-        var scrapper = new ArtistScrapper(self.soundcloud);
-        var limitArtist = 10;
-        var limitLobbies = 10;
+        var tracks = [];
 
-        Artist.find({}, function onArtists(err, artists) {
+        async.each(lobby.artists, function(artistId, artistCallback) {
+
+            self.getLastArtistSongs(artistId, function(err, artistTracks) {
+
+                tracks = tracks.concat(artistTracks);
+
+                artistCallback();
+            });
+        }, function(err) {
             if(err) return callback(err);
 
-            async.eachLimit(artists, limitArtist, forEachArtist);
-        });
+            tracks = self.sortTracks(tracks);
 
-        function forEachArtist(artist) {
-            scrapper.getMissingSounds(artist.sc_id_artist, function onMissingSongs(missingSongs) {
-                artist.getLobbies(function onLobbies(artistLobbies) {
-                    async.eachLimit(artistLobbies, limitLobbies, function(lobby) {
-                        var playlistId = lobby.sc_id_playlist;
-                        User.findById(lobby.sc_id_user, function(err, user) {
-                            self.soundCloud.addToPlaylistTop(playlistId, user, missingSongs);
-                        });
-                    });
-                });
+            self.updateLobbyTracks(lobby, tracks, function(updateError) {
+                if(updateError) return callback(updateError);
+
+                callback(null);
             });
-        }
+        });
+    }
+
+    updateLobbyTracks(lobby, tracks, callback) {
+
+        User.findById(lobby.sc_id_user, function(err, user) {
+            if(err) return callback(err);
+
+            var trackIds = _.map(tracks, 'id');
+            this.soundCloud.updatePlaylist(lobby.sc_id_playlist, user, trackIds, function(updateErr) {
+                if(updateErr) return callback(updateErr);
+
+                callback(null);
+            });
+        });
+    }
+
+    getLastArtistSongs(artistId, callback) {
+        // TODO redis, async
+        this.cacheWrapper.get('artist_songs:' + artistId, function(cacheErr, lastSongs) {
+            if(cacheErr) return callback(cacheErr);
+
+            if(lastSongs) {
+                callback(null, lastSongs);
+            }
+            else {
+                var options = {
+                    n: 20,
+                    offset: 0
+                };
+
+                var lastSCTracks = this.soundCloud.getLastArtistSongs(artistId, options);
+
+                var lastTracks = _.map(lastSCTracks, function(scTrack) {
+                    var track = {
+                        id: scTrack.id,
+                        timestamp: scTrack.timestamp // ???
+                    };
+
+                    return track;
+                });
+
+                var expiration = 200;
+
+                this.cacheWrapper.set('artist_songs:'+artistId, lastTracks, expiration);
+
+                callback(null, lastSongs);
+            }
+        });
+    }
+
+    sortTracks(tracks) {
+        return _.sort(tracks, 'timestamp');
+    }
+
+    crawlLobbies(callback) {
+        var self = this;
+        var limitLobbies = 10;
+
+        Lobby.find({}, function onLobbies(err, lobbies) {
+            if(err) return callback(err);
+
+            async.eachLimit(lobbies, limitLobbies, self.updateLobby, function(lobbyErr) {
+                if(lobbyErr) return callback(lobbyErr);
+
+                callback(null);
+            });
+        });
     }
 }
 
